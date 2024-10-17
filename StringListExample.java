dc4908079af4a793225a86fb31eba805d2b662d1
ThembarthiAkshaya <thembarthiakshaya@gmail.com>
2024-10-17 22:19:06 +0530

    Add files via upload

---
+++ StringListExample.java
@@ -0,0 +1,38 @@
+package collections_topic;
+
+import java.util.*;
+
+public class StringListExample {
+
+	public static void main(String[] args) {
+		// Create an ArrayList to hold String objects
+		List<String> list = new ArrayList();
+
+		// Adding strings to the list
+		list.add("banana");
+		list.add("Mango");
+		list.add("grapes");
+		list.add("Strawberry");
+		list.add("banana");
+
+		/*------creating iterator-------*/
+		Iterator iterator = list.iterator();
+
+		System.out.println("-----------------String List----------------");
+		// displaying the elements of the list
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+
+		/*----------performing different methods on the list-----------*/
+		System.out.println("--------------------------------");
+		System.out.println("Element at 0th position is :" + list.get(0)); // accessing a specific string
+		System.out.println("list contain strawberry :" + list.contains("Strawberry")); // Check if the list contains a
+																						// specific string
+		list.remove(2); // Remove a string from the list
+		System.out.println("After removing an element is :" + list.get(2));
+		System.out.println("Total no of elements :" + list.size()); // Get the size of the list
+		list.clear(); // Clear the list
+		System.out.println("After removing all the strings from the list:" + list.size());
+	}
+}
